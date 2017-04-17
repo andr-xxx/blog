5672d8969df1c3b648496e1e0c1f763672cb018e
andr-xxx <interdried@gmail.com>
2017-04-17 16:14:12 +0300

    first commit

---
+++ public/main/index.js
@@ -0,0 +1,34 @@
+(function () {
+  let i = 0;
+  const path = 'http://localhost:9002/api/';
+
+  $('.submit-button').on('click', () => {
+    const $usersPost = $('.users-post')
+    if (!$usersPost.val()) return
+    const data = {
+      body: $usersPost.val(),
+      date: new Date(),
+      user: `Andrey Khvesik version ${++i}`
+    };
+
+    $.post({
+      url: `${path}addPost`,
+      data: data
+    });
+
+    $usersPost.val('');
+  });
+
+  $('.remove-button').on('click', (e) => {
+    const postId = $(e.target).closest('.post-block').find('.post-id').text();
+    console.log(postId)
+
+    $.ajax({
+      type: 'DELETE',
+      url: `${path}deletePost`,
+      data: {
+        id: postId
+      },
+    })
+  })
+})();
---
+++ server.js
@@ -0,0 +1,78 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const pug = require('pug');
+const app = express();
+
+const mongoClient = require("mongodb").MongoClient;
+const objectId = require("mongodb").ObjectID;
+const url = "mongodb://localhost:27017/usersdb";
+app.set('view engine', 'pug');
+
+app.use('/public', express.static(__dirname + '/public'));
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.get('/', (req, res) => {
+  renderAllPosts(req, res);
+});
+
+app.get('/home', (req, res) => {
+  renderAllPosts(req, res);
+});
+
+app.get('/contact', (req, res) => {
+  res.render(__dirname + `/public/contact/contact.pug`, {
+    title: 'Contacts',
+    author: 'Andrey',
+    phone: '(050) 71 39 416',
+    menu: ['home', 'contact', 'about']
+  })
+});
+
+app.get('/about', (req, res) => {
+  res.render(__dirname + `/public/about/about.pug`, {
+    title: 'About',
+    textAbout: 'My first NodeJS application',
+    menu: ['home', 'contact', 'about']
+  })
+});
+
+app.post('/api/addPost', (req, res) => {
+  const post = req.body;
+
+  mongoClient.connect(url, (err, db) => {
+    db.collection("posts").insertOne(post, (err, result) => {
+
+      if(err) return res.status(400).send();
+
+      renderAllPosts(req, res);
+      db.close();
+    });
+  });
+});
+
+app.delete('/api/deletePost', (req, res) => {
+  // const id = req.body.id;
+  const id = new objectId(req.body.id);
+  mongoClient.connect(url, (err, db) => {
+    db.collection("posts").findOneAndDelete({_id: id}, (err, result) => {
+
+      if (err) return res.status(400).send();
+      console.log(result)
+      renderAllPosts(req, res);
+      db.close();
+    });
+  });
+});
+app.listen(9002);
+
+function renderAllPosts(req, res) {
+  mongoClient.connect(url, (err, db) => {
+    db.collection("posts").find({}).toArray((err, posts) => {
+      res.render(__dirname + `/public/main/index.pug`, {
+        menu: ['home', 'contact', 'about'],
+        posts: posts,
+      });
+      db.close();
+    });
+  });
+}
